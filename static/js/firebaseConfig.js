38b3a1581c2856fbae383ba493c003a7a0f1a248
likesun <like.sun@m2k.com.tw>
2020-09-14 12:39:14 +0800

    v 1.2.1

---
+++ static/js/firebaseConfig.js
@@ -0,0 +1,9 @@
+
+export default {
+  apiKey: "AIzaSyAhN19xmIlA6BEfabfi-TtdwIDUz9KzWao",
+  authDomain: "buyfarm-718c5.firebaseapp.com",
+  databaseURL: "https://buyfarm-718c5.firebaseio.com",
+  projectId: "buyfarm-718c5",
+  storageBucket: "buyfarm-718c5.appspot.com",
+  messagingSenderId: "773273072322"
+}
